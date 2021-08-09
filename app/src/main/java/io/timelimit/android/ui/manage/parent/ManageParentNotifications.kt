dc0661918d8d3fd98e86c781795da481d0103b7d
Jonas Lochmann <git@inkompetenz.org>
2021-08-09 02:00:00 +0200

    Fix reseting the wrong checkbox in the parent notification settings

--- app/src/main/java/io/timelimit/android/ui/manage/parent/ManageParentNotifications.kt
+++ app/src/main/java/io/timelimit/android/ui/manage/parent/ManageParentNotifications.kt
@@ -1,5 +1,5 @@
 /*
- * TimeLimit Copyright <C> 2019 Jonas Lochmann
+ * TimeLimit Copyright <C> 2019 - 2021 Jonas Lochmann
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -71,7 +71,7 @@ object ManageParentNotifications {
                     ) {
                         // it worked
                     } else {
-                        view.manipulationCheckbox.isChecked = tasks
+                        view.taskCheckbox.isChecked = tasks
                     }
                 }
             }
